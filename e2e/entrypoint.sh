33f448c55142285d122320500792df4011f3a765
rick <LinuxSuRen@users.noreply.github.com>
2024-07-10 10:18:07 +0000

    add e2e testing during ci

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -6,10 +6,10 @@ mkdir -p /root/.config/atest
 mkdir -p /var/data
 
 echo "start to run server"
-nohup atest server
+nohup atest server&
 cmd="atest run -p test-suite.yaml"
 
 echo "start to run testing: $cmd"
-kind=orm target=postgres:5432 driver=postgres $cmd
+kind=orm target=mysql:3306 driver=mysql $cmd
 
 cat /root/.config/atest/stores.yaml
